41a1e1a070c274c421f2ef809c0ba8d937d9819f
Levi Hauck <levihauck@Levis-MacBook-Air.local>
2026-03-30 22:42:34 -0700

    added pestchart and peststable outlines, updated gitignore

---
+++ src/frontend/gdd-frontend/src/components/PestsChart.jsx
@@ -0,0 +1,2 @@
+// TODO: add a chart to track the levels of pests for a given field over time,
+// with the ability to filter by pest type
---
+++ src/frontend/gdd-frontend/src/components/PestsTable.jsx
@@ -0,0 +1,3 @@
+// TODO: make a table to show the pest samples by field, with a dropdown to select the field,
+// and then show the pest samples for that field, with columns for sample date, location,
+// pest type, severity, etc., and the ability to sort/filter by date, pest type, severity, etc.
